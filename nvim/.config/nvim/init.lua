2ee7734e206267eede1f0a66a826fe19f01c50eb
Eugenio Cano-Manuel Mendoza <eugenio@statistica.capital>
2024-04-29 09:24:30 +0100

    rust-analyzer: ignore target, ansible, git

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -558,7 +558,19 @@ require("lazy").setup({
 				-- clangd = {},
 				-- gopls = {},
 				-- pyright = {},
-				-- rust_analyzer = {},
+				rust_analyzer = {
+					settings = {
+						["rust-analyzer"] = {
+							files = {
+								excludeDirs = {
+									".git",
+									"target",
+									"ansible",
+								},
+							},
+						},
+					},
+				},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 				--
 				-- Some languages (like typescript) have entire language plugins that can be useful:
